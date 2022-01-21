7ef198b9b21b60305ae51c422f498c070c42de95
Rohail Shah <rohails786@gmail.com>
2022-01-21 15:33:36 -0500

    updated Live site for ICE-2

--- Scripts/app.js
+++ Scripts/app.js
@@ -12,6 +12,43 @@
             // redirect to about page
             location.href = "about.html";
         });
+
+        // Step 1 get a reference to an entry point(s) (insertion point / deletion point)
+        let MainContent = document.getElementsByTagName("main")[0];
+        let DocumentBody = document.body;
+        
+        // Step 2 create an element(s) to insert
+        let MainParagraph = document.createElement("p");
+        let Article = document.createElement("article");
+        let ArticleParagraph = `<p id="ArticleParagraph" class="mt-3">This is the Article Paragraph</p>`;
+
+        // Step 3 configure new element
+        MainParagraph.setAttribute("id", "MainParagraph");
+        MainParagraph.setAttribute("class", "mt-3");
+        MainParagraph.textContent = "This is the Main Paragraph";
+        Article.setAttribute("class", "container");
+
+        // Step 4 add / insert new element
+        MainContent.appendChild(MainParagraph);
+        Article.innerHTML = ArticleParagraph;
+        DocumentBody.appendChild(Article);
+
+
+        // Deletion example
+        //document.getElementById("ArticleParagraph").remove();
+
+        // Insert Before example
+        // let NewH1 = document.createElement("h1");
+        // NewH1.setAttribute("class", "display-1");
+        // NewH1.textContent = "Hello, World!";
+        // MainContent.before(NewH1);
+
+        
+
+
+        
+
+
     }
 
     // named function
